259ce1b55fa27c704ac85caa1da99b2c1ec46a46
Mikhail German <mikhail.german@icloud.com>
2023-02-05 13:32:43 +0300

    added 3 views and 1 button

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    
+    @IBOutlet weak var yellowLightView: UIView!
+    
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
